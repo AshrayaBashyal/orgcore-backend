70d063ad81a22cd54325a4b8edbe3563296823e5
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-22 16:06:11 +0545

    feat(users): plug url endpoints for authentication

--- src/ats/urls.py
+++ src/ats/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("auth/", include("apps.users.urls")),
+    path("orgcore/", include("apps.orgcore.urls")),
 ]

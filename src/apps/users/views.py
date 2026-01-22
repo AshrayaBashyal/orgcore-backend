472a2fd2a77b69dc531c89463f1b01e69366a8cd
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-22 16:04:07 +0545

    feat(users): create views for user register, login and profile with jwt

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, ProfileSerializer
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "email": user.email
+        })
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get("email", "").lower()
+        password = request.data.get("password", "")
+
+        user = authenticate(username=email, password=password)
+        if not user:
+            return Response({"detail": "Invalid credentials"}, status=400)
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+            "email": user.email
+        })
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = ProfileSerializer(request.user)
+        return Response(serializer.data)

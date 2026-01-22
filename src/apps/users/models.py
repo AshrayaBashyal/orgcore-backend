25bcc9f61fe5ee1df1a25966c237a39da7385d25
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-22 14:09:40 +0545

    Create register and profile serializers

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework import serializers
 
-# Create your models here.
+
+class RegisterSerializer(serializers.Serializer):
+    email=serializers.EmailField()
+    password=serializers.CharField(min_length=6, write_only=True)
+
+    def create_user(self, validated_data):
+        email = self.validated_data["email"].lower()
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("Email already registered!")
+        user = User.objects.create_user(
+            username=email,
+            email=email,
+            password=validated_data["password"]
+        )    
+        return user
+    
+
+class ProfileSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "date_joined")

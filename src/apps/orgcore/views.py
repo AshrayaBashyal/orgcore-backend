cb2b5077545cc33515b4308f77a5a2843c69593a
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-23 16:12:40 +0545

    feat: add API view for company creation with service layer integration

--- src/apps/orgcore/views.py
+++ src/apps/orgcore/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Company, CompanyInvite
+from .serializers import (
+    CompanyCreateSerializer,
+    InviteCreateSerializer,
+    InviteAcceptSerializer
+)
+from .services import CompanyService
+
+
+class CompanyCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = CompanyCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        company = CompanyService.create_company(
+            owner=request.user,
+            **serializer.validated_data
+        )
+
+        return Response({"id": company.id, "name": company.name})

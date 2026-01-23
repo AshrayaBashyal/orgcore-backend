89dae060aa83e1953a5e645c16bf359902187267
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-01-23 17:34:20 +0545

    register endpoints for company and invitations

--- src/apps/orgcore/urls.py
+++ src/apps/orgcore/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path, include
+from django.urls import path
+from .views import (
+    CompanyCreateView,
+    InviteCreateView,
+    InviteCancelView,
+    InviteAcceptView,
+)
 
 urlpatterns = [
-
+    path("companies/create/", CompanyCreateView.as_view()),
+    path("companies/<int:company_id>/invite/", InviteCreateView.as_view()),
+    path("companies/<int:company_id>/invites/<int:invite_id>/cancel/", InviteCancelView.as_view()),
+    path("invites/accept/", InviteAcceptView.as_view()),
 ]
